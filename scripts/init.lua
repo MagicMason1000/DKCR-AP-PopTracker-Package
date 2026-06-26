9bfcef8b0cfd59ca68e9d662d7fae2b94f7753e9
Mason <magicmason1000@gmail.com>
2026-06-26 17:38:59 -0500

    Clean up init.lua.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,27 +1,16 @@
--- entry point for all lua code of the pack
--- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
 ENABLE_DEBUG_LOG = true
--- get current variant
 local variant = Tracker.ActiveVariantUID
--- check variant info
 IS_ITEMS_ONLY = variant:find("itemsonly")
 
-print("-- Example Tracker --")
-print("Loaded variant: ", variant)
+print("")
+print("-- Donkey Kong Country Returns — Archipelago PopTracker Package --")
+print(string.format("Loaded variant: %s", variant))
 if ENABLE_DEBUG_LOG then
-    print("Debug logging is enabled!")
+    print("DEBUG: Logging is enabled! You can change this in init.lua.")
 end
 
 -- Utility Script for helper functions etc.
 ScriptHost:LoadScript("scripts/utils.lua")
-
--- Logic
--- ScriptHost:LoadScript("scripts/logic/logic.lua")
-
--- Custom Items
-ScriptHost:LoadScript("scripts/custom_items/class.lua")
-ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlus.lua")
-ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlusWrapper.lua")
 
 -- Items
 Tracker:AddItems("items/items.jsonc")
